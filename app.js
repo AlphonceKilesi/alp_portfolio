ca0e09b806054b06c4bbf238ff7fa3dc38bf570e
Alphonce Sapuda <157350705+AlphonceKilesi@users.noreply.github.com>
2024-02-14 11:25:32 +0300

    ok

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var typed = new Typed(".effect",{
+    strings:[
+        "Banker","Developer",
+        "Consultant", "Designer",
+        "Freelancer"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
